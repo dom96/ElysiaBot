6e10e483313038f7ef431cf58cde739d2db87990
dom96 <dominikpicheta@googlemail.com>
2010-08-26 20:28:00 +0100

    Some command line argument parsing. Running as daemon doesn't seem to be working.

--- src/elysia.hs
+++ src/elysia.hs
@@ -5,11 +5,48 @@ import Data.Either
 import qualified Data.ByteString.Char8 as B
 import qualified Data.Map as M
 import Control.Concurrent.MVar
-import Language.Haskell.Interpreter (InterpreterError)
+import System.Console.GetOpt
+import System.Environment
+import System.Posix.Daemonize
+import System.Exit
+
 import Modules
 import Config
 import Users
 import Commands
+
+data Options = Options
+ {  
+   optDaemon      :: Bool
+ , optShowVersion :: Bool
+ } deriving Show
+ 
+defaultOptions    = Options
+ { optDaemon      = False
+ , optShowVersion = False
+ }
+
+options :: [OptDescr (Options -> Options)]
+options = 
+    [ Option ['v'] ["version"] 
+        (NoArg (\opts -> opts { optShowVersion = True })) "show version number"
+    , Option ['d'] ["daemon"] 
+        (NoArg (\opts -> opts { optDaemon = True })) "Run as Daemon"
+    ]
+
+parseOpts :: Options -> IO ()
+parseOpts xs
+    | optShowVersion xs = do putStrLn "ElysiaBot 0.1.0"
+                             exitSuccess
+    | otherwise         = return ()
+
+header = "Usage: elysia [options]"
+
+elOpts :: [String] -> IO (Options, [String])
+elOpts args =
+    case getOpt RequireOrder options args of 
+        (xs, n, []) -> return (foldl (flip id) defaultOptions xs, n)
+        (_, _, errs) -> ioError (userError (concat errs ++ usageInfo header options))
 
 confToIRCConf :: String -> String -> String -> ConfigServer -> ([IrcEvent] -> IrcConfig)
 confToIRCConf nick usr real confServ =
@@ -23,7 +60,7 @@ connectServers events = do
   
   lConn False (cnfServers conf !! 0)
 
-main = do
+main2 = do
   let mods = loadMods "modules"
   
   -- Load the users
@@ -34,4 +71,15 @@ main = do
   argsMVar <- newMVar (MessageArgs mods users [])
   
   let events = [(Privmsg (onMessage argsMVar)), (Privmsg (onPrivateMessage argsMVar)), (Numeric (collectServers argsMVar))]
-  connectServers events
+  _ <- connectServers events
+  return ()
+
+main = do
+  cmdArgs <- getArgs
+  (opts, _) <- elOpts cmdArgs
+  parseOpts opts
+  if optDaemon opts
+    then daemonize main2
+    else main2
+  
+
