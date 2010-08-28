94a642306e3aec2c12ac22f4408e23fbe8b14182
dom96 <dominikpicheta@googlemail.com>
2010-08-28 17:41:11 +0100

    Added a pidfile option to the config file. Added -S flag which stops the elysia that is running in the background.

--- src/Config.hs
+++ src/Config.hs
@@ -4,6 +4,8 @@ import Data.ConfigFile
 import Data.List
 import Data.Char
 import Data.Either
+import System.Directory
+import System.FilePath ((</>))
 import "mtl" Control.Monad.Error
 
 data ConfigServer = ConfigServer
@@ -16,6 +18,7 @@ data ConfigInfo = ConfigInfo
   { cnfNick    :: String
   , cnfUser    :: String
   , cnfReal    :: String
+  , cnfPidFile :: String
   , cnfServers :: [ConfigServer]
   } deriving Show
 
@@ -35,10 +38,12 @@ getData cp = do
     nick <- get cp "DEFAULT" "nick"
     user <- get cp "DEFAULT" "user"
     real <- get cp "DEFAULT" "real"
+    pidFile <- get cp "DEFAULT" "pidfile"
+    readPid <- liftIO (readFilename "pidfile" pidFile False)
     
     servers <- mapM (getSections cp) (sections cp)
     
-    return $! ConfigInfo nick user real servers
+    return $! ConfigInfo nick user real readPid servers
 
   return $ handleError config
   
@@ -68,6 +73,16 @@ readLst opt xs
     (noSpace first) : (readLst opt $ drop 1 $ dropWhile (/= ',') second)
     where (first, second) = break (== ',') xs
           noSpace f       = takeWhile (/= ' ') (dropWhile (== ' ') f)
+
+readFilename :: String -> String -> Bool -> IO FilePath
+readFilename opt xs check = do
+  currDir <- getCurrentDirectory
+  exists <- doesFileExist $ currDir </> xs
+  
+  if not exists && check
+    then ioError $ userError $ "Path does not exist in \"" ++ opt ++ "\""
+    else return $ currDir </> xs
+    
 
 forceEither :: Show e => Either e a -> a
 forceEither (Left x) = error (show x)
--- src/elysia.hs
+++ src/elysia.hs
@@ -2,13 +2,18 @@
 import Network.SimpleIRC
 import Data.Maybe
 import Data.Either
-import qualified Data.ByteString.Char8 as B
-import qualified Data.Map as M
-import Control.Concurrent.MVar
-import System.Console.GetOpt
-import System.Environment
+import System.IO (writeFile, readFile)
 import System.Exit
+import System.FilePath ((</>))
+import System.Directory
+import System.Environment
+import System.Posix.Signals
+import System.Posix.Process (getProcessID)
+import System.Console.GetOpt
 import System.Posix.Daemonize
+import Control.Concurrent.MVar
+import qualified Data.Map as M
+import qualified Data.ByteString.Char8 as B
 
 import Modules
 import Config
@@ -20,25 +25,30 @@ data Options = Options
  {  
    optDaemon      :: Bool
  , optShowVersion :: Bool
+ , optStop        :: Bool
  } deriving Show
  
 defaultOptions    = Options
  { optDaemon      = False
  , optShowVersion = False
+ , optStop        = False
  }
 
 options :: [OptDescr (Options -> Options)]
 options = 
     [ Option ['v'] ["version"] 
-        (NoArg (\opts -> opts { optShowVersion = True })) "show version number"
+        (NoArg (\opts -> opts { optShowVersion = True })) "Show version number"
     , Option ['d'] ["daemon"] 
         (NoArg (\opts -> opts { optDaemon = True })) "Run as Daemon"
+    , Option ['S'] ["stop"]
+        (NoArg (\opts -> opts { optStop = True })) "Stop"
     ]
 
 parseOpts :: Options -> IO ()
 parseOpts xs
     | optShowVersion xs = do putStrLn "ElysiaBot 0.1.0"
                              exitSuccess
+    | optStop xs        = do stopElysia
     | otherwise         = return ()
 
 header = "Usage: elysia [options]"
@@ -67,36 +77,56 @@ connectServers events conf = do
   
   lConn False (cnfServers conf !! 0)
 
+writePID conf = do
+  pid <- getProcessID
+  writeFile (cnfPidFile conf) (show pid)
+
+isRunning conf = do
+  exists <- doesFileExist (cnfPidFile conf)
+  if exists
+    then do putStrLn "Elysia is already running.\nUse ./Elysia --stop to stop."
+            exitSuccess
+    else return ()
+
+stopElysia = do
+  conf <- readConfig $ "elysia.ini"
+  pid <- readFile (cnfPidFile conf)
+  signalProcess sigTERM (read $ (lines pid) !! 0)
+  removeFile (cnfPidFile conf)
+  exitSuccess
+
 main = do
   cmdArgs <- getArgs
   (opts, _) <- elOpts cmdArgs
   parseOpts opts
   
+  currDir <- getCurrentDirectory
+  
   let mods = loadMods "modules"
   
   -- Load the users
   putStrLn "Loading users"
-  users <- readUsers "users.ini"
+  users <- readUsers $ "users.ini"
   
   -- Load the configuration
   putStrLn "Loading configuration"
-  conf <- readConfig "elysia.ini"
+  conf <- readConfig $ "elysia.ini"
   
   -- Create the MessageArgs MVar
   argsMVar <- newMVar (MessageArgs mods users [])
   
   let events = [(Privmsg (onMessage argsMVar)), (Privmsg (onPrivateMessage argsMVar)), (Numeric (collectServers argsMVar))]
-  let connect' = do _ <- connectServers events conf
+  let connect' = do writePID conf
+                    _ <- connectServers events conf
                     return ()
   
   -- All file loading(that is in elysia's directory) has to be done before calling
   -- daemonize, because it changes the current working dir.
-  -- TODO:(Maybe) I suppose i could just get the current working directory
-  -- before calling daemonize, that's messy though. This way is cleaner.
-  -- Just remember the rule, all files must be loaded before here.
   
   if optDaemon opts
-    then daemonize connect'
+    then do isRunning conf -- Exit if elysia is already running.
+            putStrLn "Starting as a daemon."
+            daemonize connect'
     else connect'
   
 
